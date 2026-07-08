237ad577da1c2d5c1f3e0f6a0ba5f5d204b00695
Sachin Kandel <sachinkandel78@gmail.com>
2026-07-08 20:37:48 +0545

    Register Author and Book models in admin with list_display

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Author, Book
 
-# Register your models here.
+# Register your models here. #Registered two model author rw book
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'date_of_birth')
+admin.site.register(Author,AuthorAdmin)
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'is_available', 'copies_available', 'copies_total')
+admin.site.register(Book, BookAdmin)
--- library/models.py
+++ library/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
-    bio = models.DateField(blank=True, null=True)
-    date_of_birth = models.TextField(blank=True, null=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.name
