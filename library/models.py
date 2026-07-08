fd5afa52508e7f0cf5ee871a31eddf54c512f07b
Sachin Kandel <sachinkandel78@gmail.com>
2026-07-08 19:33:25 +0545

    Add Author and Book models, set up .gitignore

--- library/models.py
+++ library/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.DateField(blank=True, null=True)
+    date_of_birth = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    isbn = models.CharField(max_length=13, unique=True)   #book number
+    published_date = models.DateField()
+    copies_total = models.PositiveIntegerField(default=1)
+    copies_available = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return self.title
+    
+    @property
+    def is_available(self):
+        return self.copies_available > 0
+
+
+
+
